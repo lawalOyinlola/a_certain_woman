0a6f8f219dd4fd674fb2f591ec1228128d7961f7
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2026-05-31 10:23:11 +0000

    feat(pages): compose Programs section on home and /programs; add Impact section to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { Hero } from "@/components/sections/hero";
 import { Welcome } from "@/components/sections/welcome";
 import { WhoWeServe } from "@/components/sections/who-we-serve";
 import { Pillars } from "@/components/sections/pillars";
-import { Experiences } from "@/components/sections/experiences";
-import { MenWhoStand } from "@/components/sections/men-who-stand";
+import { Programs } from "@/components/sections/programs";
 import { Manifesto } from "@/components/sections/manifesto";
 import { Founder } from "@/components/sections/founder";
+import { Impact } from "@/components/sections/impact";
 import { Events } from "@/components/sections/events";
 import { StoriesPreview } from "@/components/sections/stories";
 import { Anthem } from "@/components/sections/anthem";
@@ -26,9 +26,9 @@ export default function HomePage() {
         <Welcome />
         <WhoWeServe />
         <Pillars />
-        <Experiences />
-        <MenWhoStand />
+        <Programs layout="tabs" />
         <Manifesto />
+        <Impact />
         <Founder />
         <Events />
         <StoriesPreview />
