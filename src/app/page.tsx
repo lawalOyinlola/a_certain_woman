9ce59cddafad8f4c9e9e063cfc0860ce81943f58
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2026-06-16 08:39:47 +0000

    Remove commented-out homepage sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import { Nav } from "@/components/site/nav";
 import { Footer } from "@/components/site/footer";
 import { Hero } from "@/components/sections/hero";
-import { Welcome } from "@/components/sections/welcome";
 import { WhoWeServe } from "@/components/sections/who-we-serve";
-import { Pillars } from "@/components/sections/pillars";
-import { Programs } from "@/components/sections/programs";
 import { Manifesto } from "@/components/sections/manifesto";
 import { Founder } from "@/components/sections/founder";
 import { Impact } from "@/components/sections/impact";
-import { Events } from "@/components/sections/events";
-import { StoriesPreview } from "@/components/sections/stories";
 import { Anthem } from "@/components/sections/anthem";
 import { Join } from "@/components/sections/join";
 
@@ -23,15 +18,10 @@ export default function HomePage() {
       <Nav />
       <main id="top">
         <Hero />
-        <Welcome />
         <WhoWeServe />
-        <Pillars />
-        <Programs layout="tabs" />
         <Manifesto />
         <Impact />
         <Founder />
-        <Events />
-        <StoriesPreview />
         <Anthem />
         <Join />
       </main>
